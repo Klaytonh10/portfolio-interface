ab223def89fa8180f8a6a8ad7347dc71e8e4486c
Klaytoh10 <klaytonh10@gmail.com>
2025-11-05 11:46:40 -0600

    testing generic classes and methods

--- src/main/java/org/example/generics/FinanceApplication.java
+++ src/main/java/org/example/generics/FinanceApplication.java
@@ -3,8 +3,7 @@ package org.example.generics;
 public class FinanceApplication {
     public static void main(String[] args) {
 
-        Box<String> stringBox = new Box<String>();
-        stringBox.setTheValue("hi");
+
 
     }
 }
